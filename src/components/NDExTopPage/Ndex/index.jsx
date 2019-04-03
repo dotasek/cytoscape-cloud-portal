2b0293917057fc9b0449badfb9092ef8cfc3118c
Unknown <dotasek.dev@gmail.com>
2019-04-03 13:02:23 -0700

    Improve re-use 2

--- src/components/NDExTopPage/Ndex/index.jsx
+++ src/components/NDExTopPage/Ndex/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { withStyles } from '@material-ui/core/styles'
+import './style.css'
+
+import Split from 'react-split'
+import NetworkView from '../../Results/Ndex/NetworkView'
+import NetworkList from '../../Results/Ndex/NetworkList'
+
 import ListItemText from '@material-ui/core/ListItemText'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
@@ -7,85 +12,47 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import Avatar from '@material-ui/core/Avatar'
 import Typography from '@material-ui/core/Typography'
 
-import MenuList from '@material-ui/core/MenuList'
-
 import * as cyRESTApi from '../../../api/cyrest'
-import './style.css'
-import Sorter from '../../Results/Ndex/Sorter'
 import MenuItem from '@material-ui/core/MenuItem'
 import no_image from '../../../assets/images/no_image.png'
+/**
+ * Top page for the application
+ *
+ * @param props
+ * @returns {*}
+ * @constructor
+ */
 
-const NETWORK_SIZE_TH = 5000
+const Ndex = props => {
+  const NETWORK_SIZE_TH = 5000
 
-const styles = theme => ({
-  inline: {
-    display: 'inline'
-  },
-  subtitle: {
-    marginLeft: '1em',
-    marginTop: '0.5em'
-  },
-  networkAvatar: {
-    margin: 10,
-    color: '#fff',
-    backgroundColor: '#FAFAFA'
-  },
-  menuItem: {
-    height: '4em',
-    '&:focus': {
-      backgroundColor: 'rgba(200,205,200,0.5)'
+  const handleErrors = res => {
+    console.log('Calling!!', res)
+    if (res !== undefined) {
+      return true
     }
-  },
-  menuText: {
-    '&:focus': {}
-  },
-  secondary: {
-    width: '15em',
-    display: 'flex',
-    alignItems: 'center',
-    padding: '0.2em'
-  },
-  plot: {
-    width: '80%',
-    background: 'teal'
-  }
-})
-
-const checkCytoscapeConnection = props => {
-  //console.log(props.uiState.urlParams)
-  cyRESTApi
-    .status(
-      props.uiState.urlParams.has('cyrestport')
-        ? props.uiState.urlParams.get('cyrestport')
-        : 1234
-    )
-    .catch(e => {
-      throw Error(e)
-    })
-    .then(res => handleErrors(res))
-    .then(running => {
-      props.uiStateActions.setCytoscapeStatus(true)
-    })
-    .catch(error => {
-      props.uiStateActions.setCytoscapeStatus(false)
-    })
-}
-
-const handleErrors = res => {
-  //console.log('Calling!!', res)
-  if (res !== undefined) {
-    return true
+    return false
   }
 
-  return false
-}
-
-const NetworkList = props => {
-  const { classes, hits, sourceUUID } = props
-
-  const geneList = props.search.queryList
-
-  const id = 0 //props.search.results.jobId
+  const checkCytoscapeConnection = props => {
+    console.log(props.uiState.urlParams)
+    cyRESTApi
+      .status(
+        props.uiState.urlParams.has('cyrestport')
+          ? props.uiState.urlParams.get('cyrestport')
+          : 1234
+      )
+      .catch(e => {
+        throw Error(e)
+      })
+      .then(res => handleErrors(res))
+      .then(running => {
+        props.uiStateActions.setCytoscapeStatus(true)
+      })
+      .catch(error => {
+        props.uiStateActions.setCytoscapeStatus(false)
+      })
+  }
 
   const handleFetch = (networkUUID, nodeCount, edgeCount) => {
     props.networkActions.setNetworkSize({
@@ -177,18 +144,11 @@ const NetworkList = props => {
     )
   }
 
-  if (!hits) {
-    return <div className="network-list-wrapper" />
-  }
-
   return (
-    <div className="network-list-wrapper">
-      <Sorter />
-      <div className="network-list">
-        <MenuList>{hits.map(entry => getListItem(entry, classes))}</MenuList>
-      </div>
-    </div>
+    <Split sizes={[50, 50]} gutterSize={7} className="ndex-base">
+      <NetworkList getListItem={getListItem} {...props} />
+      <NetworkView {...props} />
+    </Split>
   )
 }
-
-export default withStyles(styles)(NetworkList)
+export default Ndex
