d036dbb3219ddbfea2365ae2d6ddd634738f3bb7
Unknown <dotasek.dev@gmail.com>
2019-04-25 14:07:12 -0700

    Add limits to recent network number, style improvements, and links

--- src/components/TopPage/RecentNetworkGrid.jsx
+++ src/components/TopPage/RecentNetworkGrid.jsx
@@ -8,9 +8,8 @@ import Typography from '@material-ui/core/Typography'
 import GridList from '@material-ui/core/GridList'
 import GridListTile from '@material-ui/core/GridListTile'
 import GridListTileBar from '@material-ui/core/GridListTileBar'
-import ListSubheader from '@material-ui/core/ListSubheader'
 import IconButton from '@material-ui/core/IconButton'
-import InfoIcon from '@material-ui/icons/Info'
+import LaunchIcon from '@material-ui/icons/Launch'
 
 const styles = theme => ({
   root: {
@@ -21,8 +20,10 @@ const styles = theme => ({
     backgroundColor: theme.palette.background.paper
   },
   gridList: {
-    width: 500,
-    height: 450
+    width: '100%',
+    'padding-left': '2em',
+    'padding-right': '2em',
+    'padding-top': '2em'
   },
   icon: {
     color: 'rgba(255, 255, 255, 0.54)'
@@ -31,7 +32,10 @@ const styles = theme => ({
 
 const RecentNetworkGrid = props => {
   useEffect(() => {
-    if (props.profiles.selectedProfile && props.ndexUiState.myNetworks == undefined) {
+    if (
+      props.profiles.selectedProfile &&
+      props.ndexUiState.myNetworks == undefined
+    ) {
       props.ndexUiStateActions.getMyNetworksStarted()
     }
     return () => {}
@@ -39,27 +43,37 @@ const RecentNetworkGrid = props => {
 
   const { classes, ndexUiState } = props
   const { myNetworks } = ndexUiState
-  const tileData = []
+
+  const handleRecentNetworkClick = networkUUID => {
+    props.history.push('/ndexAccount#' + networkUUID)
+  }
 
   return myNetworks ? (
-    <GridList cellHeight={180} className={classes.gridList}>
-      <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-        <ListSubheader component="div">Recent Networks</ListSubheader>
-      </GridListTile>
-      {tileData.map(tile => (
-        <GridListTile key={tile.img}>
-          <img src={tile.img} alt={tile.title} />
-          <GridListTileBar
-            title={tile.title}
-            subtitle={<span>by: {tile.author}</span>}
-            actionIcon={
-              <IconButton className={classes.icon}>
-                <InfoIcon />
-              </IconButton>
-            }
-          />
-        </GridListTile>
-      ))}
+    <GridList
+      cellHeight={128}
+      cellWidth={154}
+      cols={4}
+      className={classes.gridList}
+    >
+      {myNetworks
+        .slice(0, myNetworks.length > 8 ? 8 : myNetworks.length)
+        .map(network => (
+          <GridListTile
+            key={network.externalId}
+            onClick={val => handleRecentNetworkClick(network.externalId)}
+          >
+            <img />
+            <GridListTileBar
+              title={network.name}
+              subtitle={<span>Version: {network.version}</span>}
+              actionIcon={
+                <IconButton className={classes.icon}>
+                  <LaunchIcon />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
     </GridList>
   ) : null
 }
