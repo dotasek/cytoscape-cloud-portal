57ddc1f236f5215a35eec4eaec8f0622d0e31bd3
Unknown <dotasek.dev@gmail.com>
2019-04-26 15:32:44 -0700

    Summary of recent networks instead of image

--- src/components/TopPage/RecentNetworkGrid.jsx
+++ src/components/TopPage/RecentNetworkGrid.jsx
@@ -4,12 +4,15 @@ import './style.css'
 import { withStyles } from '@material-ui/core/styles'
 
 import Typography from '@material-ui/core/Typography'
-
+import Tooltip from '@material-ui/core/Tooltip'
 import GridList from '@material-ui/core/GridList'
 import GridListTile from '@material-ui/core/GridListTile'
 import GridListTileBar from '@material-ui/core/GridListTileBar'
+import ListSubheader from '@material-ui/core/ListSubheader'
 import IconButton from '@material-ui/core/IconButton'
 import LaunchIcon from '@material-ui/icons/Launch'
+
+import no_image from '../../assets/images/ndex-logo.svg'
 
 const styles = theme => ({
   root: {
@@ -23,7 +26,12 @@ const styles = theme => ({
     width: '100%',
     'padding-left': '2em',
     'padding-right': '2em',
-    'padding-top': '2em'
+    'margin-top': '2em',
+    'padding-bottom': '76px'
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)'
   },
   icon: {
     color: 'rgba(255, 255, 255, 0.54)'
@@ -50,11 +58,14 @@ const RecentNetworkGrid = props => {
 
   return myNetworks ? (
     <GridList
-      cellHeight={128}
-      cellWidth={154}
+      cellheight={128}
+      cellwidth={154}
       cols={4}
       className={classes.gridList}
     >
+      <GridListTile key="Subheader" cols={4} style={{ height: 'auto' }}>
+        <ListSubheader component="div">Recent Networks</ListSubheader>
+      </GridListTile>
       {myNetworks
         .slice(0, myNetworks.length > 8 ? 8 : myNetworks.length)
         .map(network => (
@@ -62,16 +73,27 @@ const RecentNetworkGrid = props => {
             key={network.externalId}
             onClick={val => handleRecentNetworkClick(network.externalId)}
           >
-            <img />
-            <GridListTileBar
-              title={network.name}
-              subtitle={<span>Version: {network.version}</span>}
-              actionIcon={
-                <IconButton className={classes.icon}>
-                  <LaunchIcon />
-                </IconButton>
-              }
-            />
+            <Tooltip title="Click to view in 'My Networks'" placement="bottom">
+              <GridListTileBar
+                actionIcon={
+                  <IconButton className={classes.icon}>
+                    <LaunchIcon />
+                  </IconButton>
+                }
+                title=""
+                titlePosition="bottom"
+                className={classes.titleBar}
+              />
+            </Tooltip>
+            <Typography variant="h6">{network.name}</Typography>
+            {network.description && (
+              <Typography variant="caption">{network.description}</Typography>
+            )}
+            <Typography variant="caption" color="inherit">
+              Nodes: {network.nodeCount} <br />
+              Edges: {network.edgeCount} <br />
+              Version: {network.version}
+            </Typography>
           </GridListTile>
         ))}
     </GridList>
